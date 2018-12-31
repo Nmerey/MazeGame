b82b2436c8088ce92c5d2892905270b49fc75319
Nmerey <merey.nurlan@gmail.com>
2018-12-31 22:46:57 +0100

    Make game to ask for rows and coloums of the game

--- mazegame.rb
+++ mazegame.rb
@@ -1,10 +1,32 @@
-class Mazegame
-  def initialize
+require 'gosu'
+
+  # puts "Please number of coloums"
+  # coloums = gets.chomp.to_i
+  # puts "Please enter number of rows"
+  # rows = gets.chomp.to_i
+
+class Mazegame < Gosu::Window
+  
+  def initialize coloums,rows
     
+    super coloums*40,rows*40 
+    #every cell will be height and width will be equalt to 40px
+    #so self.height = rows number and self.width = coloums number
+    self.caption = caption
+  end
+  
+  def update
+    # ...
+  end
+  
+  def draw
+    # ...
   end
 
-  def name
+  def caption
     "Mazegame"
     
   end
 end
+
+# Mazegame.new(coloums,rows).show
--- spec/mazegame_spec.rb
+++ spec/mazegame_spec.rb
@@ -2,9 +2,39 @@ require 'spec_helper'
 
 
 describe "Mazegame" do
-	describe ".name" do
-		it "returns name of game" do
-			expect(Mazegame.new.name).to eq 'Mazegame'
+	before do
+		@game_window = Mazegame.new(15,12)
+
+	end
+	
+	describe ".new" do
+		it "returns game window" do
+			expect(@game_window).to be_an_instance_of Mazegame
+
+		end
+
+		it "it is not rectangular" do
+			
+			expect(@game_window.width).not_to be @game_window.height
+			expect(@game_window.height).not_to be @game_window.width
 		end
 	end
+
+
+	describe ".caption" do
+		it "returns caption of game" do
+			expect(@game_window.caption).to eq 'Mazegame'
+		end
+	end
+	
+	describe ".draw" do
+
+	end
+
+	describe ".update" do
+
+	end
+
+	describe ""
 end
+
